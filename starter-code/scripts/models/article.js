23f28668cd06ed61044acf8d43f576a2ab115559
Rick Patci <rpatci@gmail.com>
2016-07-18 16:17:57 -0700

    push lecture starter-code

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -12,6 +12,8 @@ function Article (opts) {
  the prototype, as that would only be relevant to a single instantiated Article.
  */
 
+Article.all = [];
+
 Article.prototype.toHtml = function(scriptTemplateId) {
   var template = Handlebars.compile($(scriptTemplateId).text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
@@ -28,19 +30,34 @@ Article.prototype.toHtml = function(scriptTemplateId) {
 /* TODO: Refactor this code into a function for greater control.
     It will take in our data, and process it via the Article constructor: */
 
-ourLocalData.sort(function(a,b) {
-  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-});
-
-ourLocalData.forEach(function(ele) {
-  articles.push(new Article(ele));
-});
-
+Article.loadAll = function(dataWePassIn) {
+  dataWePassIn.sort(function(a,b) {
+    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+  }).forEach(function(ele) {
+    Article.all.push(new Article(ele));
+  });
+};
 
 /* This function below will retrieve the data from either a local or remote
  source, process it, then hand off control to the View: */
 
-
+Article.fetchAll = function() {
+  if (localStorage.hackerIpsum) {
+    /* When our data is already in localStorage:
+    1. We can process it (sort and instantiate),
+    2. Then we can render the index page. */
+    // Article.loadAll(// TODO: Invoke with our localStorage! Should we parse or stringify this?);
+    // TODO: Now let's render the index page.
+  } else {
+    /* TODO: Otherwise, without our localStorage data, we need to:
+    - Retrive our JSON file asynchronously
+     (which jQuery method method is best for this?).
+     Within this method, we should:
+     1. Load our json data,
+     2. Store that data in localStorage so we can skip the server call next time.
+     3. And then render the index page. */
+  }
+};
 
 
 
