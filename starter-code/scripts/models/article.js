91b65ddc5c33bd21c63ae39be11fae7e9880fa16
Rick Patci <rpatci@gmail.com>
2016-07-18 14:29:07 -0700

    push code for class-06

--- starter-code/scripts/models/article.js
+++ starter-code/scripts/models/article.js
@@ -11,16 +11,11 @@ function Article (opts) {
  to track, that relates to ALL of the Article objects, so it does not belong on
  the prototype, as that would only be relevant to a single instantiated Article.
  */
-Article.all = [];
 
 Article.prototype.toHtml = function(scriptTemplateId) {
   var template = Handlebars.compile($(scriptTemplateId).text());
   this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
-  if(this.daysAgo < 1) {
-    this.publishStatus = '(published today)';
-  } else {
-    this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
-  }
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
   this.body = marked(this.body);
   return template(this);
 };
@@ -32,34 +27,26 @@ Article.prototype.toHtml = function(scriptTemplateId) {
 
 /* TODO: Refactor this code into a function for greater control.
     It will take in our data, and process it via the Article constructor: */
-Article.loadAll = function(dataWePassIn) {
-  dataWePassIn.sort(function(a,b) {
-    return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
-  }).forEach(function(ele) {
-    Article.all.push(new Article(ele));
-  });
-};
+
+ourLocalData.sort(function(a,b) {
+  return (new Date(b.publishedOn)) - (new Date(a.publishedOn));
+});
+
+ourLocalData.forEach(function(ele) {
+  articles.push(new Article(ele));
+});
 
 
 /* This function below will retrieve the data from either a local or remote
  source, process it, then hand off control to the View: */
-Article.fetchAll = function() {
-  if (localStorage.hackerIpsum) {
-    /* When our data is already in localStorage:
-     1. We can process and load it,
-     2. Then we can render the index page */
-    // Article.loadAll(//TODO: Invoke with our localStorage!);
-    // TODO: Now let's render the index page.
-  } else {
-    /* TODO: Otherwise, without our localStorage data we need to:
-    - Retrieve our JSON file asynchronously
-      (which jQuery method is best for this?).
-    Within this jQuery method, we should:
-      1. Load our JSON data,
-      2. Store that same data in localStorage so we can skip the server call next time,
-      3. And then render the index page. */
-  }
-};
+
+
+
+
+
+
+
+
 
 /* Great work so far! STRETCH GOAL TIME!? Refactor your fetchAll above, or
    get some additional typing practice here. Our main goal in this part of the
--- starter-code/scripts/views/articleView.js
+++ starter-code/scripts/views/articleView.js
@@ -39,48 +39,19 @@ articleView.setTeasers = function() {
   $('h2').prev('p').remove();
   $('h2').next('p').remove();
   $('.article-body *:nth-of-type(n+2)').hide();
-
-  $('#articles').on('click', 'a.read-on', function(e) {
+  $('article').on('click', 'a.read-on', function(e) {
     e.preventDefault();
-    $(this).parent().find('*').fadeIn();
-    $(this).hide();
+    if($(this).text() === 'Read on →') {
+      $(this).parent().find('*').fadeIn();
+      $(this).html('Show Less &larr;');
+    } else {
+      $('body').animate({
+        scrollTop: ($(this).parent().offset().top)
+      },200);
+      $(this).html('Read on &rarr;');
+      $(this).parent().find('.article-body *:nth-of-type(n+2)').hide();
+    }
   });
-};
-
-articleView.initNewArticlePage = function() {
-  $('.tab-content').show();
-  $('#export-field').hide();
-  $('#article-json').on('focus', function(){
-    $(this).select();
-  });
-
-  $('#new-form').on('change', articleView.create);
-};
-
-articleView.create = function() {
-  $('#article-preview').empty();
-
-  // Instantiate an article based on what's in the form fields:
-  var formArticle = new Article({
-    title: $('#article-title').val(),
-    author: $('#article-author').val(),
-    authorUrl: $('#article-author-url').val(),
-    category: $('#article-category').val(),
-    body: $('#article-body').val(),
-    publishedOn: $('#article-published:checked').length ? new Date() : null
-  });
-
-  // Use the Handblebars template to put this new article into the DOM:
-  $('#article-preview').append(formArticle.toHtml('#article-template'));
-
-  // Activate the highlighting of any code blocks:
-  $('pre code').each(function(i, block) {
-    hljs.highlightBlock(block);
-  });
-
-  // Export the new article as JSON, so it's ready to copy/paste into blogArticles.js:
-  $('#export-field').show();
-  $('#article-json').val(JSON.stringify(formArticle) + ',');
 };
 
 articleView.renderIndexPage = function() {
@@ -97,5 +68,5 @@ articleView.renderIndexPage = function() {
   articleView.handleAuthorFilter();
   articleView.handleMainNav();
   articleView.setTeasers();
-  // TODO: start the retrieval process for our data!
 };
+// TODO: start the retrieval process for our data!
